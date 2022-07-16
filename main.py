845d2359b71c1069e3dd174e6916c3c73438fb44
aabezgin <aabezgin@greenatom.ru>
2022-07-16 13:32:09 +0700

    add db for project

--- main.py
+++ main.py
@@ -4,8 +4,30 @@ import re
 from pathlib import Path
 
 from dateutil.parser import parse
+from sqlalchemy import MetaData
+from sqlalchemy import Column
+from sqlalchemy import Date
+from sqlalchemy import Integer
+from sqlalchemy import String
+from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import Session
+from sqlalchemy.orm import relationship
 from watchdog.events import FileSystemEventHandler
 from watchdog.observers import Observer
+
+engine = create_engine('sqlite+pysqlite:///lite.db', future=True)
+Base = declarative_base(engine)
+metadata_obj = MetaData()
+
+
+class Flight(Base):
+    __tablename__ = 'flight'
+    id = Column(Integer, primary_key=True)
+    file_name = Column(String(256))
+    flt = Column(Integer)
+    depdate = Column(Date)
+    dep = Column(String)
 
 
 def from_any_date_to_iso(any_date: str):
@@ -17,6 +39,7 @@ class Handler(FileSystemEventHandler):
     def on_created(self, event):
         with open(event.src_path, 'r') as f:
             file_name = Path(event.src_path).stem
+            result_file = Path(event.src_path).parent.parent / 'Out' / f'{file_name}.json'
             parsed_file_name = re.fullmatch(
                 '(\d{8})_(\d{1,})_(.{1,})',
                 file_name
@@ -34,18 +57,30 @@ class Handler(FileSystemEventHandler):
                 if not row:
                     continue
 
-                result_obj['prl'].append({
+                person = {
                     'num': row[0],
                     'surname': row[1],
                     'firstname': row[2],
                     'bdate': from_any_date_to_iso(row[3]),
-                })
+                }
+                result_obj['prl'].append(person)
 
-        with open(Path(event.src_path).parent.parent / 'Out' / f'{file_name}.json', 'w') as f:
+                with Session(engine) as session:
+                    flight = Flight(
+                        file_name=str(result_file),
+                        flt=int(person['num']),
+                        depdate=parse(parsed_file_name[0]),
+                        dep=result_obj['dep'],
+                    )
+                    session.add(flight)
+                    session.commit()
+
+        with open(result_file, 'w') as f:
             f.write(json.dumps(result_obj))
 
 
 if __name__ == '__main__':
+    Base.metadata.create_all()
     working_directory = str(Path.cwd() / 'working_directory' / 'In')
     event_handler = Handler()
     observer = Observer()
--- setup.py
+++ setup.py
@@ -10,5 +10,6 @@ setup(
         'watchdog',
         'sqlalchemy',
         'python-dateutil',
+        'pysqlite3',
     ],
 )
