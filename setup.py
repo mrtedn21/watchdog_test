90e3471cd16f38af06721221d483f018f750c84e
aabezgin <aabezgin@greenatom.ru>
2022-07-16 09:53:19 +0700

    add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name='Watchdog test',
+    version='0.1.0',
+    description='Testing task with watchdog',
+    author='Bezgin Alexandr',
+    author_email='bezgin.sasha06@gmail.com',
+    install_requires=['watchdog', 'sqlalchemy'],
+)
